c30435d01d2fb4940ba10e646f93e0d76cc3f869
jdlrobson <jdlrobson@gmail.com>
2015-12-15 14:51:44 -0800

    handle errors in proxy.

--- proxy.php
+++ proxy.php
@@ -3,11 +3,17 @@ $title = $_GET['title'];
 $leadOnly = false;
 
 header('Content-Type: application/json');
-$url = 'http://reading-web-research.wmflabs.org/api/slim/';
+$url = 'https://reading-web-research.wmflabs.org/api/slim/';
 if ( $leadOnly ) {
 	$url .= 'lead/';
 }
 $url .= rawurlencode( $title );
-
+set_error_handler(function($code, $err) {
+	if ( strpos( $err, '404 Not Found' ) ) {
+		header("HTTP/1.0 404 Not Found");
+		echo 'Nope.';
+	}
+}, E_WARNING);
 $resp = file_get_contents( $url, false );
 echo $resp;
+restore_error_handler();
